f800ddfa0480af23eed8776a02626ac53d74fe71
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-06-17 22:40:16 +0300

    Seq class documented

--- src/seq.cpp
+++ src/seq.cpp
@@ -1,7 +1,26 @@
 #include <map>
 #include "seq.h"
 
+/*! \brief The map object to transform read type values to their names */
 std::map <ReadType, std::string> type_names;
+
+/*! \brief Initialize the map of read types and their names
+ *
+ *  The goal of read type names is to indicate the reason why a read was
+ *  filtered. The name contains the filter type and threshold which allows for
+ *  obtaining statistics on the filtered reads. The threshold
+ *  values are specified in the function arguments.
+ *
+ *  \param[in]  length  the minimum read length
+ *  \param[in]  polyG   the minimum length of a polyG region in a read
+ *  \param[in]  dust_k  this value is related to the *DustMasker* algorithm
+ *  \param[in]  dust_cutoff this value is related to the *DustMasker* algorithm
+ *
+ *  \remark The DustMasker algorithm is described in the following paper:
+ *  Morgulis, Aleksandr, E. Michael Gertz, Alejandro A. Schäffer, and Richa
+ *  Agarwala. "A fast and symmetric DUST implementation to mask low-complexity
+ *  DNA sequences." *Journal of Computational Biology* 13, no. 5 (2006): 1028-1040.
+ */
 void init_type_names(int length, int polyG, int dust_k, int dust_cutoff)
 {
     type_names[ReadType::ok] = "ok";
@@ -13,6 +32,11 @@ void init_type_names(int length, int polyG, int dust_k, int dust_cutoff)
     type_names[ReadType::dust] = "dust" + std::to_string(dust_k) + '_' + std::to_string(dust_cutoff);
 }
 
+/*! \brief Return a string representing a read type name
+ *
+ *  \param[in]  type    a read type
+ *  \return the string representing the specified read type
+ */
 const std::string & get_type_name (ReadType type) {
     return type_names[type];
 }
--- src/seq.h
+++ src/seq.h
@@ -4,23 +4,45 @@
 #include <string>
 #include <fstream>
 
+/*! \brief Criteria for read filtration */
 enum ReadType{
-    ok,
-    adapter = 1,
-    n,
-    polyG,
-    polyC,
-    length,
-    dust
+    ok,             //!< passes filtration
+    adapter = 1,    //!< contains an adapter sequence
+    n,              //!< the number of Ns surpasses the threshold
+    polyG,          //!< contains too long polyG sequence
+    polyC,          //!< contains too long polyC sequence
+    length,         //!< read length is too short
+    dust            //!< has low complexity according to the *DustMasker* model
 };
 
 void init_type_names(int length, int polyG, int dust_k, int dust_cutoff);
+
+/*! \brief Get read type name from its value
+ *
+ *  \param[in]  type    the type of a read
+ */
 const std::string & get_type_name (ReadType type);
 
+/*! \brief A sequence read with its quality information
+ *
+ *  This class implements routines to read reads from files in the FASTQ
+ *  format.
+ */
 class Seq {
 public:
-    Seq() {}
+    /*! \brief The read sequence constructor
+     *
+     *  It does nothing except for initializing class members
+     *  (Seq::id, Seq::seq and Seq::qual) with empty strings.
+     */
+    Seq() : id(""), seq(""), qual("") {}
 
+    /*! \brief Read a read from a FASTQ file
+     *
+     *  \param[in]  fin an input stream to write the read from
+     *  \return     true if a read was read, false if we reached
+     *              the file end
+     */
     bool read_seq(std::ifstream & fin)
     {
         std::string tmp;
@@ -34,6 +56,10 @@ public:
         return true;
     }
 
+    /*! \brief Write the read in the FASTQ format
+     *
+     *  \param[in]  fout    an output stream to write the read to
+     */
     void write_seq(std::ofstream & fout)
     {
         fout << id << std::endl;
@@ -42,14 +68,18 @@ public:
         fout << qual << std::endl;
     }
 
+    /*! \brief Add the read type to its ID
+     *
+     *  \param[in] type the type of a read
+     */
     void update_id(ReadType type)
     {
         id.append(":"+get_type_name(type));
     }
 
-    std::string id;
-    std::string seq;
-    std::string qual;
+    std::string id;     //!< the read ID
+    std::string seq;    //!< the read sequence
+    std::string qual;   //!< the read quality sequence
 };
 
 #endif // SEQ_H
