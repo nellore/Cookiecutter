5d3730ed45e565197177ff10df30ee0085cecfb7
Aleksey Komissarov <ad3002@gmail.com>
2015-07-23 00:34:42 +0300

    fix folder casting

--- src/run_barch_se.py
+++ src/run_barch_se.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 
     fastq_files1 = args["fastq1"].split(",")
     cpu = int(args["cpus"])
-    out_folder = int(args["out"])
+    out_folder = args["out"]
     command = args["command"]
     fragments = args["fragments"]
     polyG = args["polyG"]
