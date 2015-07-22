238e0083140f58338980980caeb044f0e974a638
Aleksey Komissarov <ad3002@gmail.com>
2015-07-22 15:04:30 +0300

    replaced message from adapter to hit

--- src/seq.cpp
+++ src/seq.cpp
@@ -24,7 +24,7 @@ std::map <ReadType, std::string> type_names;
 void init_type_names(int length, int polyG, int dust_k, int dust_cutoff)
 {
     type_names[ReadType::ok] = "ok";
-    type_names[ReadType::adapter] = "adapter";
+    type_names[ReadType::adapter] = "match";
     type_names[ReadType::n] = "n";
     type_names[ReadType::polyG] = "polyG" + std::to_string(polyG);
     type_names[ReadType::polyC] = "polyC" + std::to_string(polyG);
