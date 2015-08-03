5a76ea9b5480f18625a493c6953e26ff717e8987
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-08-03 17:26:54 +0300

    make_library script refactored

--- src/make_library.py
+++ src/make_library.py
@@ -1,68 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#@created: 10.10.2015
-#@author: Aleksey Komissarov
-#@contact: ad3002@gmail.com
+# created: 10.10.2015
+# author: Aleksey Komissarov
+# contact: ad3002@gmail.com
 
-import sys
 import argparse
 import re
 from collections import defaultdict
 
-def get_revcomp(sequence):
-    '''Return reverse complementary sequence.
 
-    >>> complementary('AT CG')
+def get_revcomp(seq):
+    """Return reverse complementary sequence.
+
+    >>> get_revcomp('AT CG')
     'CGAT'
 
-    '''
+    """
     c = dict(zip('ATCGNatcgn[]', 'TAGCNtagcn]['))
-    return ''.join(c.get(nucleotide, '') for nucleotide in reversed(sequence))
+    return ''.join(c.get(nucleotide, '') for nucleotide in reversed(
+        seq))
 
 
 def sc_iter_fasta_brute(file_name):
     """ Iter over fasta file."""
-    header = None
+    seq_header = None
     seq = []
-    with open(file_name) as fh:
-        data = fh.readlines()
+    with open(file_name) as file_handler:
+        data = file_handler.readlines()
         for line in data:
             if line.startswith(">"):
                 if seq:
-                    sequence = "".join(seq)
-                    yield (header, sequence)
-                header = line
+                    yield (seq_header, "".join(seq))
+                seq_header = line
                 seq = []
                 continue
             seq.append(line)
-        if seq or header:
-            sequence = "".join(seq)
-            yield (header, sequence)
+        if seq or seq_header:
+            yield (seq_header, "".join(seq))
 
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(description='Fasta file to kmers file.')
-	parser.add_argument('-i','--input', help='Fasta input', required=True)
-	parser.add_argument('-o','--output', help='Kmer output', required=True)
-	parser.add_argument('-k','--k', help='K lengths', required=True)
-	args = vars(parser.parse_args())
-	fasta_file = args["input"]
-	output = args["output"]
-	k = int(args["k"])
+    parser = argparse.ArgumentParser(
+        description='Create a filf of kmers from a FASTA file')
+    parser.add_argument('-i', '--input', help='FASTA input',
+                        required=True)
+    parser.add_argument('-o', '--output', help='kmer output',
+                        required=True)
+    parser.add_argument('-k', '--k', help='length of kmers',
+                        required=True)
+    args = vars(parser.parse_args())
+    fasta_file = args["input"]
+    output = args["output"]
+    k = int(args["k"])
 
-	kmers = defaultdict(int)
-	for header, sequence in sc_iter_fasta_brute(fasta_file):
-		sequence = sequence.upper()
-		sequence = re.sub("\s+", "", sequence, re.S|re.M)
-		for i in xrange(0, len(sequence)-k+1):
-			kmer = sequence[i:i+k]
-			kmers[kmer] += 1
-			rkmer = get_revcomp(kmer)
-			kmers[rkmer] += 1
-	with open(output, "w") as fh:
-		for kmer in kmers:
-			fh.write("%s\t%s\n" % (kmer, kmers[kmer]))
-
-
+    kmers = defaultdict(int)
+    for header, sequence in sc_iter_fasta_brute(fasta_file):
+        sequence = sequence.upper()
+        sequence = re.sub("\s+", "", sequence, re.S | re.M)
+        for i in xrange(0, len(sequence)-k+1):
+            kmer = sequence[i:i+k]
+            kmers[kmer] += 1
+            rkmer = get_revcomp(kmer)
+            kmers[rkmer] += 1
+    with open(output, "w") as fh:
+        for kmer in kmers:
+            fh.write("%s\t%s\n" % (kmer, kmers[kmer]))
